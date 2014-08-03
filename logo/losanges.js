b9843e1814232ebae6235bf8151a2cd771a78bf1
Robin Berjon <robin@berjon.com>
2014-08-03 17:50:40 -0400

    first W

--- logo/losanges.js
+++ logo/losanges.js
@@ -2,6 +2,37 @@
 
 // we have a 500x500 square we'd like to fill in both directions
 
+var skips = {
+    "9,4":  true // centre tip of the W
+,   "10,4": true // down to bottom left tip
+,   "11,4": true
+,   "11,3": true
+,   "12,4": true
+,   "12,3": true
+,   "13,3": true
+,   "10,5": true // down to bottom right tip
+,   "11,5": true
+,   "12,5": true
+,   "12,6": true
+,   "13,5": true
+,   "11,2": true // back up to top left tip
+,   "10,3": true
+,   "10,2": true
+,   "9,2": true
+,   "9,1": true
+,   "8,2": true
+,   "8,1": true
+,   "7,1": true
+,   "11,6": true // back up to the top right tip
+,   "10,6": true
+,   "10,7": true
+,   "9,6": true
+,   "9,7": true
+,   "8,7": true
+,   "8,8": true
+,   "7,7": true
+};
+
 function fill (el) {
     // var colours = ["#f00", "#fc0"];
     // el.setAttribute("fill", colours[(i + j) % 2]);
@@ -10,9 +41,12 @@ function fill (el) {
 
 function fillWithLosanges (el) {
     var poly = document.createElementNS(el.namespaceURI, "polygon");
-    poly.setAttribute("points", "25,0, 50,25, 25,50, 0,25, z");
-    for (var i = 0, n = 19; i < n; i++) {
+    poly.setAttribute("points", "20,0, 40,30, 20,60, 0,30, z");
+    poly.setAttribute("stroke-width", 2);
+    poly.setAttribute("stroke", "#fff");
+    for (var i = 0, n = 18; i < n; i++) {
         for (var j = 0, m = 9; j < m; j++) {
+            if (skips[[i,j].join(",")]) continue;
             var clone = poly.cloneNode(true)
             ,   delta = (i % 2) ? 25 : 0
             ;
